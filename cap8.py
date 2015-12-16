77cac21d8f1f1f73be78701a2c38e972de01dfda
tmedeirosb <thiago.mdeiros@gmail.com>
2015-12-16 19:52:54 -0300

    cap 8 - final

--- cap8.py
+++ cap8.py
@@ -2,6 +2,7 @@
 
 import decimal
 import unittest
+from unittest import mock
 
 
 class Column:
@@ -64,7 +65,7 @@ class DataTableTest(unittest.TestCase):
             self.fail("Chamada não gerou erro, mas deveria")
 """
 
-
+""""
 class DataTableTest(unittest.TestCase):
     def setUp(self):
         self.table = DataTable('A')
@@ -87,7 +88,36 @@ class DataTableTest(unittest.TestCase):
     def test_add_column_invalid_type(self):
         self.assertRaises(Exception,
         self.table.add_column, ('col', 'invalid'))
+"""
 
+
+BUFF_SIZE = 1024
+
+
+def download_length(response, output, length):
+    times = length / BUFF_SIZE
+    if times % BUFF_SIZE > 0:
+        times += 1
+    for time in range(int(times)):
+        output.write(response.read(BUFF_SIZE))
+        print('Download %d ' % ((time * BUFF_SIZE)/length)*100)
+
+
+class DownloadTest(unittest.TestCase):
+    def test_download_with_known_length(self):
+        response = mock.MagicMock()
+        response.read = mock.MagicMock(side_effect=['Data']*2)
+
+        output = mock.MagicMock()
+        download_length(response, output, 1025)
+
+        calls = [mock.call(BUFF_SIZE), mock.call(BUFF_SIZE)]
+
+        response.read.assert_has_calls(calls)
+
+        calls = [mock.call('Data'), mock.call('Data')]
+
+        output.write.assert_has_calls(calls)
 
 if __name__ == '__main__':
     unittest.main()
